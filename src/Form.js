e8f430d843484bad0f224286b9165c97b221169b
Taja House <ahlliahtaja@yahoo.com>
2020-04-30 12:05:49 -0700

    Testing

--- src/Form.js
+++ src/Form.js
@@ -1,56 +1,145 @@
-import React from 'react';
-import * as Yup from "yup";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
 
-const formSchema = Yup.object().shape({
-    name: Yup.string().required("Must include name."),
-    email: Yup.string()
-      .email("Must be a valid email address.")
-      .required("Must include email address."),
-    password: Yup.string()
-      .min(6, "Password must be at least 6 characters long.")
-      .required("Password is required"),
-    terms: Yup.boolean().oneOf([true], "You must accept the Terms and Conditions")
+const formSchema = yup.object().shape({
+  name: yup.string().required("Name is a required field."),
+  password: yup.string().min(6, "Password must be 6 characters long"),
+  email: yup
+    .string()
+    .email("Must be a valid email address.")
+    .required("Must include email address."),
+  terms: yup.boolean().oneOf([true], "please agree to terms of use"),
+
+});
+
+export default function Form() {
+  // state for whether our button should be disabled or not.
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // managing state for our form inputs
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password:"",
+    terms: ""
+
   });
 
-const Form = () =>{
-    return (
-        <form>
-            <label htmlFor="name">
-                Name
-                <input 
-                id="name"
-                type="text" 
-                name="name" 
-                />
-            </label>
-            <label htmlFor="email">
-                Email
-                <input
-                id="email" 
-                type="email" 
-                name="email"
-                />
-            </label>
-            <label htmlFor="password">
-                password
-                <input 
-                id="password" 
-                type="password" 
-                name="password" 
-                />
-            </label>
-            <label htmlFor="terms">
-                Agree to terms and conditions?
-                <input 
-                id="terms" 
-                type="checkbox" 
-                name="terms"
-                />
-            </label>
-            <button>Submit</button>
+  // state for our errors
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password:"",
+    terms: ""
+  });
 
-        </form>       
-    );
+  // new state to set our post request too. So we can console.log and see it.
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  const formSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(res => {
+        setUsers(res.data); // get just the form data from the REST api
+
+        // reset form if successful
+        setFormState({
+          name: "",
+          email: "",
+          password:"",
+          terms: ""
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  const validateChange = e => {
+    // Reach will allow us to "reach" into the schema and test only one part.
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
+  };
+
+  const inputChange = e => {
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value
+    };
+
+    validateChange(e);
+    setFormState(newFormData);
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={inputChange}
+        />
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+      </label>
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          name="email"
+          value={formState.email}
+          onChange={inputChange}
+        />
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
+      </label>
+      <label htmlFor="password">
+        Password
+        <input
+          name="password"
+          value={formState.password}
+          onChange={inputChange}
+        />
+        {errors.password.length > 0 ? (
+          <p className="error">{errors.password}</p>
+        ) : null}
+      </label>
+
+      <label htmlFor="terms" className="terms">
+      Terms & Conditions
+        <input
+          type="checkbox"
+          name="terms"
+          checked={formState.terms}
+          onChange={inputChange}
+        />
+      </label>
+      {/* displaying our post request data */}
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <button type="submit" disabled={buttonDisabled}>Submit</button>
+    </form>
+  );
 }
-
-export default Form;
