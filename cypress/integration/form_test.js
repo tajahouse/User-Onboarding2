35b04c2d57c3958adea8880dc530833a56ac1f97
Taja House <ahlliahtaja@yahoo.com>
2020-04-30 19:04:25 -0700

    Added content?

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,8 +1,8 @@
 describe('Test 1', function(){
-    this.beforeEach(function(){
+    beforeEach(function(){
         cy.visit("http://localhost:3000")
     });
-    it("test", function(){
+    it("test", () => {
         cy.get('[data-cy="name"]').type("Taja").should("have.value", "Taja")
         cy.get('[data-cy="email"]').type("taja@email.com").should("have.value", "taja@email.com")
         cy.get('[data-cy="password"]').type("password").should("have.value", "password")
